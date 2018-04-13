d25f92ad2115fbc9eca4a36dee9715f88546345c
hemant231 <hemant.chhonkar@gmail.com>
2018-04-13 13:47:35 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,48 +4,65 @@
 JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
-	echo configure_aws_cli
 	aws --version
-	aws configure set default.region us-east-2
+	aws configure set default.region us-east-1
 	aws configure set default.output json
 }
-
 deploy_cluster() {
-    echo deploy_cluster_function
-    family="test-def"
-    
-    echo deploy_cluster_function 1
-    
+
+   family="yello-family"
+
+   make_and_register_task_def
    
+    if [[ $(aws ecs update-service --cluster test-cluster --service test-service --task-definition $revision | \
+                   $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service."
+        return 1
+    fi
 
-  SERVICES=`aws ecs describe-services --services test-service --cluster test-cluster --region us-east-2 | jq .failures[]`
-REVISION=`aws ecs describe-task-definition --task-definition ${family} --region us-east-2 | jq .taskDefinition.revision`
-
-
-echo $SERVICES
-echo $REVISION
-
- if [ "$SERVICES" == "" ]; then
-   echo "entered existing service"
-   DESIRED_COUNT=`aws ecs describe-services --services test-service --cluster test-cluster --region us-east-2 | jq .services[].desiredCount`
-   echo $DESIRED_COUNT;
-   if [ ${DESIRED_COUNT} = "0" ]; then
-     DESIRED_COUNT="1"
-   else
-     DESIRED_COUNT="0"
-   aws ecs update-service --cluster test-cluster --region us-east-2 --service test-service --task-definition ${family}:${REVISION} --desired-count ${DESIRED_COUNT}
-    DESIRED_COUNT="1"
-   fi
-   sleep 20
-   aws ecs update-service --cluster test-cluster --region us-east-2 --service test-service --task-definition ${family}:${REVISION} --desired-count ${DESIRED_COUNT}
- else
-   echo "entered new service"
-   aws ecs create-service --service-name test-service --desired-count 1 --task-definition ${family} --cluster test-cluster --region us-east-2
- fi
- 
+    # wait for older revisions to disappear
+    # not really necessary, but nice for demos
+    for attempt in {1..30}; do
+        if stale=$(aws ecs describe-services --cluster test-cluster --services test-service | \
+                       $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
+            echo "Waiting for stale deployments:"
+            echo "$stale"
+            sleep 5
+        else
+            echo "Deployed!"
+            return 0
+        fi
+    done
+    echo "Service update took too long."
+    return 1
 }
 
-
+make_and_register_task_def(){
+	task_template='[
+		{
+			"name": "go-sample-webapp",
+			"image": "$AWS_ACCOUNT_ID.dkr.ecr.us-east-2.amazonaws.com/yello-team:$CIRCLE_SHA1",
+			"essential": true,
+			"memory": 500,
+			"cpu": 2,
+			"portMappings": [
+				{
+					"containerPort": 8081,
+					"hostPort": 8081
+				}
+			]
+		}
+	]'
+	
+	task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1)
+	
+	if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+        	echo "Revision: $revision"
+	else
+		echo "Failed to register task definition"
+		return 1
+	fi
+}
 push_ecr_image(){
 	echo push_ecr_image
 	eval $(aws ecr get-login --region us-east-2)

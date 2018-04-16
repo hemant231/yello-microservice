596612716722e8c09cc6ad977434b5c119e2cb46
hemant231 <hemant.chhonkar@gmail.com>
2018-04-16 15:47:43 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,61 +4,48 @@
 JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
+	echo configure_aws_cli
 	aws --version
-	aws configure set default.region us-east-1
+	aws configure set default.region us-east-2
 	aws configure set default.output json
 }
+
 deploy_cluster() {
-
-   family="yello-family"
-
-   make_and_register_task_def
-
-   aws ecs list-task-definitions
-
-   #aws ecs create-service --cluster fargate-cluster --service-name fargate-service --task-definition sample-fargate:1 --desired-count 2 --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-abcd1234],securityGroups=[sg-abcd1234]}"
+    echo deploy_cluster_function
+    family="test-def"
+    
+    echo deploy_cluster_function 1
+    
    
-    if [[ $(aws ecs create-service --cluster test-cluster --service-name yello-ms-service --task-definition $revision | \
-                   $JQ '.service.taskDefinition') != $revision ]]; then
-        echo "Error updating service."
-        return 1
-    fi
 
-    #list services
+  SERVICES=`aws ecs describe-services --services test-service --cluster test-cluster --region us-east-2 | jq .failures[]`
+REVISION=`aws ecs describe-task-definition --task-definition ${family} --region us-east-2 | jq .taskDefinition.revision`
 
-    aws ecs list-services --cluster test-cluster
 
-    # wait for older revisions to disappear
-    # not really necessary, but nice for demos
-    for attempt in {1..30}; do
-        if stale=$(aws ecs describe-services --cluster test-cluster --services yello-ms-service | \
-                       $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
-            echo "Waiting for stale deployments:"
-            echo "$stale"
-            sleep 5
-        else
-            echo "Deployed!"
-            return 0
-        fi
-    done
-    echo "Service update took too long."
-    return 1
+echo $SERVICES
+echo $REVISION
+
+ if [ "$SERVICES" == "" ]; then
+   echo "entered existing service"
+   DESIRED_COUNT=`aws ecs describe-services --services test-service --cluster test-cluster --region us-east-2 | jq .services[].desiredCount`
+   echo $DESIRED_COUNT;
+   if [ ${DESIRED_COUNT} = "0" ]; then
+     DESIRED_COUNT="1"
+   else
+     DESIRED_COUNT="0"
+   aws ecs update-service --cluster test-cluster --region us-east-2 --service test-service --task-definition ${family}:${REVISION} --desired-count ${DESIRED_COUNT}
+    DESIRED_COUNT="1"
+   fi
+   sleep 20
+   aws ecs update-service --cluster test-cluster --region us-east-2 --service test-service --task-definition ${family}:${REVISION} --desired-count ${DESIRED_COUNT}
+ else
+   echo "entered new service"
+   aws ecs create-service --service-name test-service --desired-count 1 --task-definition ${family} --cluster test-cluster --region us-east-2
+ fi
+ 
 }
 
-make_and_register_task_def(){
 
-	echo $HOME
-	echo registering task
-
-	#aws ecs register-task-definition --cli-input-json file://$HOME/tasks/fargate-task.json
-	
-	if revision=$(aws ecs register-task-definition --cli-input-json file://$HOME/yello-microservice/yello-ms-task.json | $JQ '.taskDefinition.taskDefinitionArn'); then
-        	echo "Revision: $revision"
-	else
-		echo "Failed to register task definition"
-		return 1
-	fi
-}
 push_ecr_image(){
 	echo push_ecr_image
 	eval $(aws ecr get-login --region us-east-2)
